cf99dcc28fc7103c0b07bdcab6a71e5941c0e1dd
bchap1n <brockchapin@live.com>
2022-06-02 10:13:43 -0700

    📝 docs'n'links

--- publish.ps1
+++ publish.ps1
@@ -1,2 +1,4 @@
+
+# Build the module with Press first, then publish via manual Github Action trigger on main
 $modulePath = "$PSScriptroot\BuildOutput\Proofpoint.TAP"
 Publish-Module -Path $ModulePath -NuGetApiKey $Env:PS_GALLERY_KEY
